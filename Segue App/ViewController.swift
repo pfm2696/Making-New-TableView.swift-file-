e5b0475dd5a5df94165ff78b6c53d3bba201310c
Peter Munzo <pfm2696@gmail.com>
2015-02-17 03:21:50 -0800

    Segue App

---
+++ Segue App/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Segue App
+//
+//  Created by Peter & Liz  on 2/17/15.
+//  Copyright (c) 2015 P&L Studios. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
